94400b993b18fbbdfd6536a8cef2579bd8af8b4b
Rodevp <rodriguezrodrigor12@gmail.com>
2022-12-04 13:00:54 -0500

    backdroop animated and component

--- mortyapp/components/ListCharacters.tsx
+++ mortyapp/components/ListCharacters.tsx
@@ -1,19 +1,22 @@
 import { useEffect, useRef, useState } from 'react'
 import {
     View,
-    Text,
     FlatList,
     ListRenderItemInfo,
     Dimensions,
     Animated,
     SafeAreaView,
-    StyleSheet } from 'react-native'
+    StyleSheet,
+    Image } from 'react-native'
 import { ICharacter } from '../models/character'
 import { getCharacterView } from '../views/getCharacterView'
 import CharacterItem from './CharacterItem'
 
 interface Props {}
-
+interface PropsBack {
+  scrollX: Animated.AnimatedInterpolation<string | number>,
+  images: Array<string>
+}
 
 const WIDTH = Dimensions.get("window").width
 const HEIGHT = Dimensions.get("window").height
@@ -21,6 +24,51 @@ const WIDTH_CONTENT = WIDTH * 0.7
 const SPACE = 10
 const SPACE_CONTENT = (WIDTH - WIDTH_CONTENT) / 2
 const HEIGHT_BACKDROOP = HEIGHT * 0.5
+
+
+const BackDroop = ({scrollX, images}: PropsBack ) => {
+
+ 
+  return (
+    <View
+      style={[
+        {height: HEIGHT_BACKDROOP, width: WIDTH, position: "absolute", top: 0},
+        StyleSheet.absoluteFillObject
+      ]}
+    >
+      {
+        images.map((image, index) => {
+          
+          const inputRange = [
+            (index - 1) * WIDTH_CONTENT,
+            index * WIDTH_CONTENT,
+            (index + 1) * WIDTH_CONTENT
+          ]
+        
+          const outputRange = [0, 1, 0]
+        
+          const opacity = scrollX.interpolate({
+            inputRange,
+            outputRange
+          })
+          
+          return (
+            <Animated.Image
+            source={{uri: image}}
+            key={index}
+            style={[
+              {height: HEIGHT_BACKDROOP, width: WIDTH, position: "absolute", top: 0},
+              { opacity: opacity }
+            ]}
+          />
+          )
+        })
+      }
+    </View>
+  )
+}
+
+
 
 const ListCharacters = (props: Props) => {
 
@@ -35,11 +83,16 @@ const ListCharacters = (props: Props) => {
 
     }, [])
 
+    const allImages = listCharacters.map(character => character.image)
+
   return (
     <SafeAreaView
       style={styles.container}
     >
-      <Text>ListCharacters</Text>
+      <BackDroop
+        images={allImages}
+        scrollX={scrollX}
+      />
       <View>
         <Animated.FlatList
             data={listCharacters}
@@ -73,11 +126,11 @@ const ListCharacters = (props: Props) => {
             showsHorizontalScrollIndicator={false}
             contentContainerStyle={{
               paddingTop: 200,
-              marginHorizontal: SPACE_CONTENT
+              paddingHorizontal: SPACE_CONTENT
             }}
             decelerationRate={0}
             snapToInterval={WIDTH_CONTENT}
-            scrollEventThrottle={8}
+            scrollEventThrottle={16}
             onScroll={Animated.event(
               [{ nativeEvent: { contentOffset: { x: scrollX } } }],
               { useNativeDriver: true }
